95caf121bd8e57407ef5189d72ca29faaef07b93
1900608 Elliot Stevenson <1900608@uad.ac.uk>
2020-03-19 09:22:17 +0000

    Update BallManager.h

---
+++ Week9/CMP105App/BallManager.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Ball.h"
+#include <math.h>
+#include <vector>
+#include <iostream>
+
+class BallManager
+{
+public:
+	BallManager();
+	~BallManager();
+
+	void spawn();
+	void update(float dt);
+	void deathCheck();
+	void render(sf::RenderWindow* window);
+
+private:
+	std::vector<Ball> balls;
+	sf::Vector2f spawnPoint;
+	sf::Texture texture;
+};
+
